87a0df70179c302707c255087444e0e5cd1beec2
swk <swk1072@gmail.com>
2025-02-19 18:16:32 +0900

    updated in moblie

--- public/scr.js
+++ public/scr.js
@@ -12,20 +12,4 @@ function mOve(uRl) {
     }
 }
 
-const dDay = new Date("2024-01-14T00:00:00").setHours(0, 0, 0, 0); 
 
-function updateCountdown() {
-  const now = new Date().setHours(0, 0, 0, 0); 
-  const daysLeft = Math.floor((dDay - now) / (1000 * 60 * 60 * 24));
-  const countdownElement = document.getElementById("d-day-countdown");
-  const container = document.getElementById("d-day-container");
-
-  if (daysLeft < 0) {
-    if (container) container.remove();  
-  } else {
-    if (countdownElement) countdownElement.innerText = `D-${daysLeft}`;
-  }
-}
-
-setInterval(updateCountdown, 1000);
-updateCountdown();
