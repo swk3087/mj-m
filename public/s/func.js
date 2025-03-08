5c700557d2a59cd9f15872222ef52e7027689d69
swk <swk1072@gmail.com>
2025-03-08 16:45:37 +0900

    updated in moblie

--- public/s/func.js
+++ public/s/func.js
@@ -1,4 +1,4 @@
-function mOve(uRl) {
+function mOve(uRl) {        //모달창에서 쓰니까 지우면 안됨
     function isMobile() {
       var userAgent = navigator.userAgent;
       var mobile = /(iPhone|iPad|Android|BlackBerry|Windows Phone)/i.test(userAgent);
@@ -20,25 +20,13 @@ function Goo(URl) {
 function dowN(url) {
     const a = document.createElement('a');
     a.href = url;
-
-    // URL에서 파일명 추출 (마지막 '/' 이후 문자열)
-    const filename = url.split('/').pop().split('?')[0]; // 쿼리스트링 제거
-
-    a.download = filename; // 파일명 지정
+    const filename = url.split('/').pop().split('?')[0];
+    a.download = filename; 
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
 }
 
-const $body = document.querySelector('body');
-function preventScroll(e) {
-  e.preventDefault();
-}
-// 'wheel' 이벤트를 사용하여 스크롤 감지 후 방지
-$body.addEventListener('wheel', preventScroll, { passive: false });
-$body.addEventListener('click', function() { // body 를 다시 클릭하면 스크롤 재개
-  $body.removeEventListener('wheel', preventScroll, { passive: false });
-});
 
 class linkClass extends HTMLElement {
   connectedCallback() {
