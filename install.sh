fe60a7289f1cc9389fc01988cb2a8bd77d73dfc5
seo won kyeom <swk1072@gmail.com>
2025-06-06 17:06:37 +0900

    updated in moblie

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 pkg up -y
 pkg install nodejs-lts python git gh -y
-npm config set bin-links false
-npm install -g firebase-tools --force --legacy-peer-deps --no-optional
+npm config set bin-links true
+npm install -g firebase-tools --no-optional
+export PATH="$HOME/.npm-global/bin:$PATH"
+echo 'export PATH="$HOME/.npm-global/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+ln -s $HOME/.npm-global/lib/node_modules/firebase-tools/lib/bin/firebase.js $PREFIX/bin/firebase
+chmod +x $PREFIX/bin/firebase
 git config --global --add safe.directory /storage/emulated/0/dev/.mj-m
 git config --global user.email "swk1072@gmail.com"
 git config --global user.name "seo won kyeom"
